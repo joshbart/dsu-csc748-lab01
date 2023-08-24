b6438b1ce6989295d318895edd0b28444e0aa77e
Joshua Bartholomew <joshua.bartholomew@trojans.dsu.edu>
2023-08-24 16:39:27 -0500

    Clarifying that this is local and not remote

--- lab1-1.py
+++ lab1-1.py
@@ -1,7 +1,7 @@
 import pwn
 
 if __name__ == "__main__":
-    victim_process = pwn.process("./lab1-1.bin")
+    local_victim_process = pwn.process("./lab1-1.bin")
 
-    victim_process_standard_output = victim_process.recv()
+    victim_process_standard_output = local_victim_process.recv()
     print(victim_process_standard_output)
