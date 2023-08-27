7f650652a29b9d8fa8194ac3fb0b9edb030dca5d
Joshua Bartholomew <joshua.bartholomew@trojans.dsu.edu>
2023-08-27 16:44:20 -0500

    Getting rid of useless STDOUT

--- lab1-3.py
+++ lab1-3.py
@@ -10,3 +10,7 @@ if __name__ == "__main__":
     # process_to_exploit = pwn.remote("csc748.hostbin.org", 7013)
     # This next line can replace the one above for testing against a local binary.
     process_to_exploit = pwn.process("./lab1-3.bin")
+
+    # I read a line from STDOUT until the hexadecimal prefix is reached.
+    # This information is not useful, so it is dropped.
+    process_to_exploit.recvuntil(b"0x")
