68a09629aa583c646155dd5f689776c44970dd85
Joshua Bartholomew <joshua.bartholomew@trojans.dsu.edu>
2023-08-26 22:42:11 -0500

    Added local testing capability.

--- lab1-3.py
+++ lab1-3.py
@@ -1,4 +1,8 @@
 import pwn
 
 if __name__ == "__main__":
-    process_to_exploit = pwn.remote("csc748.hostbin.org", 7013)
+
+    # I run the process.
+    # process_to_exploit = pwn.remote("csc748.hostbin.org", 7013)
+    # This next line can replace the one above for testing against a local binary.
+    process_to_exploit = pwn.process("./lab1-3.bin")
